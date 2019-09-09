e0acfd3dceec6308f1ef05da5b544fd36b8368dd
John Leschinski <picard102@gmail.com>
2019-09-08 21:38:33 -0400

    Campfire 5

---
+++ src/campfire5.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Live Show submission confirmation
+ *
+ * @category PHP
+ * @package  @theme_folder@
+ * @author   @author_name@  <@author_email@>
+ * @version  Release: @package_version@
+ * @link     @git_link@
+ */
+
+?><!DOCTYPE html>
+<html class="no-js">
+<head>
+  <meta name="format-detection" content="date=no">
+  <meta name=”x-apple-disable-message-reformatting”>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php
+    $css = file_get_contents('style.css');
+    echo '<style>'.$css.'</style>';
+    $css = file_get_contents('campfire.css');
+    echo '<style>'.$css.'</style>';
+  ?>
+</head>
+<body>
+  <!--*|IF:MC_PREVIEW_TEXT|*-->
+<!--[if !gte mso 9]><!----><span class="mcnPreview Text" style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;">*|MC_PREVIEW_TEXT|*</span><!--<![endif]-->
+<!--*|END:IF|*-->
+<div style="display:none; white-space:nowrap; font:15px courier; color:#ffffff; line-height:0; width:600px !important; min-width:600px !important; max-width:600px !important;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
+
+<table class="table-body">
+
+
+  <tr>
+    <td class="main-header camp-hero">
+      <table><tr><td>
+              <img src="http://podcamptoronto.com/external/img/raster/2019/emails/smore.gif" height="50px">
+
+      <h1 class="num">5</h1>
+      <p class="days">Days</p>
+      <p class="left">Left</p>
+
+      <a href="https://www.eventbrite.ca/e/campfire-2019-tickets-65250900199?aff=mc1" class="button">Register Now&nbsp;<span>&#x25ba;&#xFE0E;</span></a><br/>
+
+
+<p>Only a few more days to get your tickets to the fall's best podcasting and campfire adjacent event!</p>
+      </td></tr></table>
+    </td>
+  </tr>
+
+
+    <tr>
+    <td class="logos-sponsor">
+      <?php
+        require'templates/sponsors.php';
+      ?>
+    </td>
+  </tr>
+
+
+  <tr>
+    <td class="tree-content">
+    </td>
+  </tr>
+
+
+
+
+  <tr>
+    <td class="main-footer">
+      <?php
+        require'templates/footer.php';
+      ?>
+    </td>
+  </tr>
+
+
+
+
+
+
+
+</table>
+</body></html>
